495357f880f6f6565baa1efa1e502b158f08749d
Paul Hsueh <hsueh.paul@gmail.com>
2021-04-15 16:34:10 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(arr, event) {
+    const writeCardsArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        writeCardsArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+    return writeCardsArr;
+}
+
+function countDown(num){
+    const count = 0;
+    while (count <= num) {
+        console.log(num);
+        debugger;
+        num--;
+    }
+}
